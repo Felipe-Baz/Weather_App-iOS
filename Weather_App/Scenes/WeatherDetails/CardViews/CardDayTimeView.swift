fde0ae6f87bfc69c49c2020989e8f3ba48a0fa95
Felipe Baz <baz@xds.com.br>
2022-07-22 19:08:22 -0300

    Add Launch screen

---
+++ Weather_App/Scenes/WeatherDetails/CardViews/CardDayTimeView.swift
@@ -0,0 +1,20 @@
+//
+//  CardDayTimeView.swift
+//  Weather_App
+//
+//  Created by Felipe Baz on 22/07/22.
+//
+
+import UIKit
+
+class CardDayTimeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Weather_App/Scenes/WeatherDetails/CardViews/CardFogInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  CardFogInfoView.swift
+//  Weather_App
+//
+//  Created by Felipe Baz on 22/07/22.
+//
+
+import UIKit
+
+class CardFogInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
