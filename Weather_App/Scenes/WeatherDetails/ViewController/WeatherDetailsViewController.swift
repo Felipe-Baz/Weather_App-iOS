50dd5a0eb3e95ace88d28badfe94fb25fa8a58df
Felipe Baz <baz@xds.com.br>
2022-07-28 23:59:15 -0300

    change sunrise time and sunset time with api values

--- Weather_App/Scenes/WeatherDetails/ViewController/WeatherDetailsViewController.swift
+++ Weather_App/Scenes/WeatherDetails/ViewController/WeatherDetailsViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 
 class WeatherDetailsViewController: UIViewController, WeatherResponse {
+    
     var service: WeatherService?
+    
+    // MARK: Fields in screen
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var temperatureDetailsLabel: UILabel!
-    @IBOutlet weak var umidadeLabel: UILabel!
-    @IBOutlet weak var pressaoLabel: UILabel!
+    @IBOutlet weak var sunriseTime: UILabel!
+    @IBOutlet weak var sunsetTime: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+    @IBOutlet weak var humidity: UILabel!
+    @IBOutlet weak var windSpeed: UILabel!
+    @IBOutlet weak var visibility: UILabel!
+    
     
     func onSuccess(response: WeatherModel) {
         print(response)
@@ -41,6 +49,8 @@ class WeatherDetailsViewController: UIViewController, WeatherResponse {
         print("reload Card")
         formatTemperature(weather: res)
         formatTemperatureDetails(weather: res)
+        formatTimesDetails(weather: res)
+        formatPressureDetails(weather: res)
     }
     
     func getInfo() {
@@ -73,8 +83,24 @@ class WeatherDetailsViewController: UIViewController, WeatherResponse {
         temperatureDetailsLabel.text = label
     }
     
+    private func formatTimesDetails(weather: WeatherModel) {
+        debugPrint("Format Times Details")
+        sunsetTime.text = formatTime(with: Double(weather.sys.sunset))
+        sunriseTime.text = formatTime(with: Double(weather.sys.sunrise))
+    }
+    
     private func kelvinToCelsius(kelvin: Float) -> Int {
         let celsius = kelvin - 273.15
         return Int(celsius)
+    }
+    
+    private func formatTime(with timestamp: Double) -> String {
+        let date = Date(timeIntervalSince1970: timestamp)
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateFormat = "HH:mm"
+        let strDate = dateFormatter.string(from: date)
+        return strDate
     }
 }
